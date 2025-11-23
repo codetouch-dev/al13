7c6faaf7f2111bc2917b7289a6122c1202a178a5
Codetouch_ <98784499+codetouch-dev@users.noreply.github.com>
2025-11-23 14:59:29 +0200

    added dynamic header background

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -2,8 +2,6 @@
 const burgerBtn = document.querySelector('.burger-menu')
 const bodyElem = document.querySelector('body')
 
-
-
 document.addEventListener('click', (event) => {
   const target = event.target
   if (target === burgerBtn || target.parentElement === burgerBtn) {
@@ -11,5 +9,15 @@ document.addEventListener('click', (event) => {
 
     bodyElem.classList.toggle('body-menu--opened')
   }
+});
 
+// for header on scroll
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
 });
